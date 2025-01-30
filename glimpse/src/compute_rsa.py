699f17118b3f6126c1f9f2418c9ce25fa5ba9eb5
Franci1101 <francescofasano11@gmail.com>
2025-01-30 12:57:28 +0100

    Update compute_rsa.py

--- glimpse/src/compute_rsa.py
+++ glimpse/src/compute_rsa.py
@@ -54,6 +54,9 @@ def parse_summaries(path: Path) -> pd.DataFrame:
 def compute_rsa(summaries: pd.DataFrame, model, tokenizer, device):
     results = []
     for name, group in tqdm(summaries.groupby(["id"])):
+        print(f"Processing document with id {name}")
+        print("Group contents:")
+        print(group)
         rsa_reranker = RSAReranking(
             model,
             tokenizer,
