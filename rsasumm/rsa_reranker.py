4827dad40640660e6b9ef92adce037ba0b61fa95
Franci1101 <francescofasano11@gmail.com>
2025-01-31 20:04:25 +0100

    Update rsa_reranker.py

--- rsasumm/rsa_reranker.py
+++ rsasumm/rsa_reranker.py
@@ -233,7 +233,9 @@ class RSAReranking:
         # Se best_rsa1 è una lista di stringhe, la trasformiamo in una lista di liste di stringhe
         if isinstance(best_rsa1, list) and isinstance(best_rsa1[0], str):
             best_rsa1 = [[sentence] for sentence in best_rsa1]
-        
+        print("best_rsa1:", best_rsa1)
+        print("best_rsa2:", best_rsa2)
+
         # Ora puoi procedere con l'unione delle due liste
         best_rsa = [list(set(best + additional)) for best, additional in zip(best_rsa1, best_rsa2)]
 
