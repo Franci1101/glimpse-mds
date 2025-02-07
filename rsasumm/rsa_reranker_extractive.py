f2fd6ac9ee34fe08738e9dfe1d55c5040a923f0d
Franci1101 <francescofasano11@gmail.com>
2025-02-07 10:03:32 +0100

    Update rsa_reranker_extractive.py

--- rsasumm/rsa_reranker_extractive.py
+++ rsasumm/rsa_reranker_extractive.py
@@ -221,7 +221,7 @@ class RSAReranking:
             initital_consensuality_score,
             consensuality_scores,
         ) = self.mk_listener_dataframe(t=t)
-        print(f"Numero totale di frasi candidate: {len(self.candidates)}")
+        
         top_k = 25  # Numero di frasi migliori da selezionare per recensione
         best_rsa = speaker_df.apply(lambda x: x.nlargest(top_k).index.tolist(), axis=1).values
 
