c2a175bd0cff9bc9820b84fb58ae7b9f76e2276d
Franci1101 <francescofasano11@gmail.com>
2025-02-10 21:29:24 +0100

    Update output_data_processing.py

--- glimpse/src/output_data_processing.py
+++ glimpse/src/output_data_processing.py
@@ -36,7 +36,8 @@ def process_rsa(file_path, output_csv="summaries.csv"):
             best_rsa_value = [best_rsa_value]
         
        # Create a unique summary by concatenating the candidates
-        generated_summary = " ".join(sentence.replace("\n", "").replace(";", ",") for sentence in best_rsa_value)
+        generated_summary = " ".join(str(sentence).replace("\n", "").replace(";", ",") for sentence in best_rsa_value)
+        #generated_summary = " ".join(sentence.replace("\n", "").replace(";", ",") for sentence in best_rsa_value)
         gold_summary = gold_summary.replace(";", ",")
 
         # Save the results for each document
