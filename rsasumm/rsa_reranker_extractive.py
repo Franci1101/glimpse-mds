c4a5c6411eda5a26758936d5eb7dbd73bccc0db0
Franci1101 <francescofasano11@gmail.com>
2025-02-10 17:56:21 +0100

    Update rsa_reranker_extractive.py

--- rsasumm/rsa_reranker_extractive.py
+++ rsasumm/rsa_reranker_extractive.py
@@ -224,7 +224,7 @@ class RSAReranking:
 
 
        
-        top_k = 20  # Numero massimo di frasi da selezionare per recensione
+        top_k = 15  # Numero massimo di frasi da selezionare per recensione
 
         best_rsa = speaker_df.apply(lambda x: x.nlargest(top_k).index.tolist(), axis=1).values
 
